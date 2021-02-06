aaa923dfb27097d2f8d30527cd59b538ed2687c6
Tipu <tipu.dany@gmail.com>
2021-02-07 01:45:37 +0600

    Vuex Practice

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,42 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from "axios";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        counter:0,
+        colorCode:'green',
+    },
+    mutations: {
+        decreaseCounter(state){
+            state.counter--
+        },
+        increaseCounter(state,randomNumber){
+            state.counter+= randomNumber
+        },
+        setColorCode(state,newValue){
+            state.colorCode = newValue
+        }
+    },
+    actions: {
+        increaseCounter({ commit }){
+            axios.get('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+                .then(response =>{
+                    console.log(response.data)
+                    commit('increaseCounter',response.data)
+                })
+        },
+        setColorCode({commit},newValue){
+            commit('setColorCode', newValue)
+        }
+    },
+    getters:{
+        counterSquire(state){
+            return state.counter * state.counter
+        }
+    },
+    modules: {
+    }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <div
+            :style="{ color: $store.state.colorCode}"
+            class="counter"
+        >
+            {{$store.state.counter}}
+        </div>
+
+        <div class="counter-squire">
+            {{$store.state.counter}} <sup>2</sup> = {{$store.getters.counterSquire}}
+        </div>
+        <button @click="$store.commit('decreaseCounter')"> - </button>
+        counter
+        <button @click="$store.dispatch('increaseCounter')"> + </button>
+        <br>
+        <input
+            v-model="colorCode"
+            placeholder="Enter color code"
+            type="text"
+        >
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    name: 'Home',
+    data(){
+        return{
+
+        }
+    },
+    computed:{
+        colorCode:{
+            get(){
+                return this.$store.state.colorCode
+            },
+            set(newValue){
+                this.$store.dispatch('setColorCode',newValue)
+            }
+        }
+    }
 }
 </script>
+<style>
+button{
+    padding: 10px;
+    font-size: medium;
+    margin: 10px;
+}
+.counter{
+    font-size: xxx-large;
+    margin: 10px;
+}
+</style>
